64f41f044f1829f44ccc2d7c7b0119c745dd4269
Stephen Kinnaird <slkinnaird@gmail.com>
2022-08-22 10:41:23 +0100

    html templates created

--- repositories/merchant_repository.py
+++ repositories/merchant_repository.py
@@ -3,15 +3,15 @@ from db.run_sql import run_sql
 from models.merchant import Merchant
 from models.tag import Tag
 
-# def create(merchant):
-#     sql = """
-#     INSERT INTO merchants ( name, type ) VALUES ( %s, %s ) returning id
-#     """
-#     values = [merchant.name]
-#     results = run_sql(sql, values)
-#     merchant.id = results[0]['id']
-#     return merchant
+def create(merchant):
+    sql = """
+    INSERT INTO merchants ( name, type ) VALUES ( %s, %s ) returning id
+    """
+    values = [merchant.name]
+    results = run_sql(sql, values)
+    merchant.id = results[0]['id']
+    return merchant
 
-# def delete_all():
-#     sql = "DELETE FROM merchants"
-#     run_sql(sql)
+def delete_all():
+    sql = "DELETE FROM merchants"
+    run_sql(sql)
--- repositories/tag_repository.py
+++ repositories/tag_repository.py
@@ -4,6 +4,13 @@ from models.tag import Tag
 from models.merchant import Merchant
 from models.transaction import Transaction
 
+def create(tag):
+    sql = "INSERT INTO tags( name ) VALUES ( %s ) RETURNING id"
+    values = [tag.name]
+    results = run_sql( sql, values )
+    tag.id = results[0]['id']
+    return tag
+
 def delete_all():
     sql = "DELETE FROM tags"
     run_sql(sql)
